054e70d9492cba7085a161406eac6f2488dfa3f7
Phoelix <Lalala007gi!>
2018-08-18 06:02:40 -0400

    work with db
    Signed-off-by: Phoelix <Lalala007gi!>

--- core.py
+++ core.py
@@ -1,13 +1,16 @@
 
+import RU
+import re
 import logging
 import requests
 from SQLite import SQLite
+from sqlite3 import Error
 from configparser import SafeConfigParser
 from telegram import (ReplyKeyboardMarkup, ParseMode, ReplyKeyboardRemove)
 from telegram.ext import (Updater,CommandHandler,MessageHandler,Filters,RegexHandler,
                           ConversationHandler)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(filename="WORKLOG.log", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
@@ -15,35 +18,68 @@ logger = logging.getLogger(__name__)
 parser = SafeConfigParser()
 parser.read('options.conf')
 
+INPUT,WALLET = range(2)
+
+temp = ()
+
 def start(bot, update):
-    keyboard = [[]]
+    keyboard = [[RU.buyBTC_btn]]
+    update.message.reply_text(str(RU.welcome), reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+    logger.info("User %s start the game." % update.message.from_user.first_name)
+    return INPUT
+
+
+def cancel (bot,update):    # /cancel function
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation."%user.first_name)
+    update.message.reply_text(str(RU.bye_a).format(user.first_name), reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+def error (bot,update,error):       # if the bot slove error in update
+    logger.warn('Update "%s" caused error "%s"'%(update,error))
+
+
+def inp(bot, update):
+    global temp
+    user = update.message.from_user
+    db = SQLite()
+    try:
+        i ={'userID': temp[int(user.id)]}
+    except :
+        try:
+            i = {'userID': int(db.get_member(user.id))}
+        except :
+            logger.warn('User "%s", error "%s"' % (user.id, error))
+            try:
+                db.add_member(data=(user.id, user.name, user.first_name))
+                i = { 'userID': user.id}
+            except Error:
+                logger.warn('User "%s", error "%s"' % (user.id, error))
+                return INPUT
+    reg =  re.search(r'[A-Za-z0-9]{34,40}', update.message.text).group(0)
+    if re.match(r'[A-Za-z0-9]{34,40}', update.message.text):
+        result = requests.get('https://blockchain.info/rawaddr/'+reg).json()
+        i['wallet'] = reg
+        print(result)
+        logger.info("User %s input wallet." % user.first_name)
+    logger.info("User %s input end block." % user.first_name)
+    update.message.reply_text(RU.inputVal)
+    return INPUT
 
 
 
 def main ():            # workplace
-    updater = Updater(token)
+    updater = Updater(parser.get('core', 'token'))
     dp = updater.dispatcher
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
-            Q1:[RegexHandler(nextreg,q1),
+            INPUT:[
+                MessageHandler(Filters.text, inp),
                 CommandHandler('start',start),
                 CommandHandler('cancel',cancel)],
-
-            CHECKANSWER:[
-                RegexHandler(regex_answer,check),
-                CommandHandler('start', start),
-                CommandHandler('cancel',cancel)],
-
-            GOAL:[
-                RegexHandler(Gresultsreg,goal),
-                CommandHandler('start', start),
-                CommandHandler('cancel',cancel)],
-
-            RESTART: [
-                CommandHandler('start', start),
-                CommandHandler('cancel',cancel)]
         },
 
         fallbacks=[CommandHandler('cancel',cancel)]
