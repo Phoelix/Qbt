2c9fc4371b60c12679e201a9eb90b6621bffe5db
Phoelix <Lalala007gi!>
2018-08-21 13:50:04 +0300

    Updated conversation phrases and core
    Signed-off-by: Phoelix <Lalala007gi!>

--- core.py
+++ core.py
@@ -52,19 +52,24 @@ def error (bot,update,error):       # if the bot slove error in update
 
 def inp(bot, update):
     user = update.message.from_user
-    global temp
-
-    if  re.match(r'[A-Za-z0-9]{34,40}', update.message.text):
-        reg = re.match(r'[A-Za-z0-9]{34,40}', update.message.text).group(0)
+    text = update.message.text
+    db = SQLite()
+    if  re.match(r'[A-Za-z0-9]{34,40}', text):               # BTC Wallet
+        reg = re.match(r'[A-Za-z0-9]{34,40}', text).group(0)
         result = requests.get('https://blockchain.info/rawaddr/'+reg).json()
-        i = Member.Member(user.id)
-        i.add_wall(reg)
-        temp.append(i)
         print(reg)
         logger.info("User %s input wallet." % user.first_name)
-    elif re.match(r'[0-9]{13}',update.message.text):
-        reg = re.search(r'[0-9]{13}', update.message.text).group(0)
-        temp[user.id].add_trID(reg)
+    elif re.match(r'[0-9]{13}', text):                        # QIWI Transaction
+        reg = re.search(r'[0-9]{13}', text).group(0)
+
+    elif 1000.0<float(text)<10000.0 or 0.001<float(text)<10:                          # RUB to exchange TODO regexp to find value
+        update.message.reply_text(str(RU.inputVal))
+        sql = 'INSERT INTO transact_temp (tgID, RUB, BTC) VALUES(?,?,?) ON CONFLICT(tgID) DO UPDATE SET RUB = excluded.RUB, BTC = excluded.BTC'
+        if 1000.0<float(text)<10000.0:
+            btc =
+            data = [int(user.id), float(text), ]
+        db.use_your_power(sql, data)
+
     logger.info("User %s input end block." % user.first_name)
     update.message.reply_text(RU.inputVal)
     return INPUT
